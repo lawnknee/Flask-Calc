3b599267f503e1bce643bda22274cd7aeab0f81c
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-07-07 11:57:42 -0700

    we're so good

--- app.py
+++ app.py
@@ -1,1 +1,53 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, div, mult
+
+app = Flask(__name__)
+
+OPERATIONS = {
+    "add" : add,
+    "sub" : sub,
+    "div" : div,
+    "mult" : mult
+}
+
+@app.route('/math/<operation>')
+def big_math(operation):
+    """Takes in numbers a, b, and operation from URL query
+    performs operation on a and b"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(OPERATIONS[operation](a,b))
+
+@app.route('/add')
+def add_nums():
+    """Add numbers a and b given from URL query"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(add(a,b))
+
+@app.route('/sub')
+def sub_nums():
+    """Subtract number b from a given from URL query"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(sub(a,b))
+
+@app.route('/div')
+def div_nums():
+    """Divide number a by b given from URL query"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(div(a,b))
+
+
+@app.route('/mult')
+def mult_nums():
+    """Multiply numbers a and b given from URL query"""
+
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(mult(a,b))
